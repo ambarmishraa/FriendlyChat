043c4f43f9eeadd94754712e85de740ffc7ed784
ambarmishraa <ambarmishra740@gmail.com>
2024-02-21 15:43:11 +0530

    Friendly Chat Ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
